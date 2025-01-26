76c62db6e3e5bc7e36cedd8ede20632a90e5b487
alenior <alenior@gmail.com>
2025-01-26 18:30:07 -0300

    Images

--- P6/PDM/minha_loja/lib/main.dart
+++ P6/PDM/minha_loja/lib/main.dart
@@ -37,13 +37,47 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.network(src)
+            Row(
+              children: [
+                Image.network(
+                  'https://raw.githubusercontent.com/alenior/IFCE/refs/heads/main/P6/PDM/minha_loja/lib/media/esp32.webp',
+                  width: 150,
+                  height: 150,
+                ),
+                Text('Esp32'),
+                Text('Microcontrolador poderoso com acesso à internet via wifi.'),
+              ],
+            ),
+            Row(
+              children: [
+                Image.network(
+                  'https://raw.githubusercontent.com/alenior/IFCE/refs/heads/main/P6/PDM/minha_loja/lib/media/protoboard830.webp',
+                  width: 150,
+                  height: 150,
+                ),
+                Text('Protoboard'),
+                Text('Matriz de 830 furos para conexão de condutores sem necessitar soldar.'),
+              ],
+            ),
+            Row(
+              children: [
+                Image.network(
+              'https://raw.githubusercontent.com/alenior/IFCE/refs/heads/main/P6/PDM/minha_loja/lib/media/fonte5v.webp',
+                  width: 150,
+                  height: 150,
+                ),
+                Text('Fonte 5v'),
+                Text('Fonte de tensão 5v e 3v3 por pinos e USB.'),
+              ],
+            ),
           ],
         ),
+      ),
       ),
     );
   }
